d32ff9eeb86d31f26620ecff6e2ab3868c4d8e01
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-11 09:52:58 +0200

    Extend authentication function structure

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,13 @@ import os
 from fastapi import FastAPI
 from app.setup import *
 
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI()
 
 
---
+++ app/utilities/authenticating.py
@@ -0,0 +1,8 @@
+
+def authenticate_user(email: str, password: str):
+    # 1. Find user with email
+    # 2. If no user -> return False
+    # 3. Check if password matches
+    # 4. If not match -> return False
+    # 5. Return user model
+    pass
