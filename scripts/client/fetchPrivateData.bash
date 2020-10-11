8095273f1a64f8ddfc7448bfeab41345bc2b1e31
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-11 11:30:51 +0200

    Add script to fetch private data

---
+++ scripts/client/fetchPrivateData.bash
@@ -0,0 +1,5 @@
+
+access_token="..."
+
+curl -X GET 'http://localhost:8000/profile' \
+    --header "Authorization: Bearer "$access_token
--- scripts/client/obtainToken.bash
+++ scripts/client/obtainToken.bash
@@ -1,8 +1,9 @@
 
 # Execute from the project root directory with
-# "bash scripts/client/obtainToken.bash"
-curl -X POST "http://localhost:8000/token" \
-    -H  "accept: application/json" \
-    -H  "Content-Type: application/x-www-form-urlencoded" \
-    -d "username=...&password=..." \
+# 'bash ./scripts/client/obtainToken.bash'
+
+curl -X POST 'http://localhost:8000/token' \
+    -H  'accept: application/json' \
+    -H  'Content-Type: application/x-www-form-urlencoded' \
+    -d 'username=...&password=...' \
     > ./scripts/client/jwt.json
