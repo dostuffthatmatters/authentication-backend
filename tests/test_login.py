64366b8a4221bef41b675565d932884853df3023
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-12 19:16:46 +0200

    Comment on tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,25 +5,31 @@ TEST_ACCOUNT = {"email": "c", "password": "000000d!"}
 
 
 def test_login(client):
+    # Login with not-registered account
     response = client.post("/login", data=TEST_ACCOUNT)
     assert(response.status_code == 401)
 
+    # Registered that account
     response = client.post("/register", data=TEST_ACCOUNT)
     assert(response.status_code == 200)
 
+    # Login with registered account
     response = client.post("/login", data=TEST_ACCOUNT)
     assert(response.status_code == 200)
 
+    # Check whether response has the right format
     content_dict = get_content_dict(response)
     assert(all([key in content_dict for key in ["access_token", "token_type"]]))
     assert(64 < len(content_dict["access_token"]) < 256)
     assert(content_dict["token_type"] == "bearer")
 
+    # Try to get private data with invalid access_token
     response = client.post("/account", data={
         "access_token": "123"
     })
     assert(response.status_code == 401)
 
+    # Try to get private data with valid access_token
     response = client.post("/account", data={
         "access_token": content_dict["access_token"]
     })
