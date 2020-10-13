6d0d0868e6d5df2d27d7f749cc68832de336bc17
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-13 23:04:36 +0200

    Finish mail sending script

--- app/setup.py
+++ app/setup.py
@@ -21,7 +21,7 @@ assert(all([
     isinstance(os.getenv(env_var), str) for env_var in [
         'ENVIRONMENT', 'MONGO_DB_CONNECTION_STRING', 'PASSWORD_SALT',
         'SECRET_KEY', 'HASH_ALGORITHM', 'ACCESS_TOKEN_EXPIRE_MINUTES',
-        'MAILGUN_API_KEY'
+        'MAILGUN_API_KEY', 'ADMIN_FRONTEND_URL', 'AUTHENTICATION_BACKEND_URL'
     ]
 ]))
 assert(os.getenv('ENVIRONMENT') in ['production', 'development', 'testing'])
@@ -33,5 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 HASH_ALGORITHM = os.getenv('HASH_ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY')
+ADMIN_FRONTEND_URL = os.getenv('ADMIN_FRONTEND_URL')
+AUTHENTICATION_BACKEND_URL = os.getenv('AUTHENTICATION_BACKEND_URL')
 
 ENVIRONMENT = os.getenv('ENVIRONMENT') if ENVIRONMENT is None else ENVIRONMENT
