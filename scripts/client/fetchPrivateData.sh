c6a82cd3b150d4342355bea5839db5617773fff4
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-15 01:23:13 +0200

    Remove unused scripts

--- scripts/client/fetchPrivateData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-access_token="..."
-
-curl -X GET 'http://localhost:8000/profile' \
-    --header "Authorization: Bearer "$access_token
--- scripts/secretKey.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Generate a secret key
-openssl rand -hex 64
