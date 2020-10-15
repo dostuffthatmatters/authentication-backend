9c7e89e368f497a67717e27906346b9cc58a5191
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-15 21:14:35 +0200

    Implement forgot-password mail

--- app/utilities/mailing.py
+++ app/utilities/mailing.py
@@ -7,21 +7,21 @@ async def send_verification_mail(db_account):
         'from': 'FastSurvey <noreply@admin.fastsurvey.io>',
         'to': db_account["email"],
         'subject': 'Please verify your FastSurvey account',
-        'html': generate_verification_email_html(db_account),
+        'html': verification_email_html(db_account),
         'o:testmode': ENVIRONMENT == 'testing'
     }
     response = await httpx_client.post('/messages', data=data)
     assert(response.status_code == 200), f"{response}"
 
 
-def generate_verification_email_html(db_account):
+def verification_email_html(db_account):
     assert("email_token" in db_account)
     verification_url = (
-        ADMIN_FRONTEND_URL + "/verification?token=" + db_account["email_token"]
+        ADMIN_FRONTEND_URL + "/verify-email?token=" + db_account["email_token"]
     )
     return (
         '<h1>Welcome to FastSurvey!</h1>' +
-        '<p>Please verify this email vie the following link:</p>' +
+        '<p>Please verify this email via the following link:</p>' +
         f'<a href="{verification_url}" target="_blank">{verification_url}</a>' +
         '<p>Best,<br/>The FastSurvey Team</p>' +
         '<p><br/>If you have not signed up for this ' +
@@ -30,5 +30,26 @@ def generate_verification_email_html(db_account):
 
 
 async def send_forgot_password_mail(email: str, token: str):
-    "to be implemented"
-    pass
+    data = {
+        'from': 'FastSurvey <noreply@admin.fastsurvey.io>',
+        'to': email,
+        'subject': 'Please verify your FastSurvey account',
+        'html': forgot_password_email_html(token),
+        'o:testmode': ENVIRONMENT == 'testing'
+    }
+    response = await httpx_client.post('/messages', data=data)
+    assert(response.status_code == 200), f"{response}"
+
+
+def forgot_password_email_html(token: str):
+    verification_url = (
+        ADMIN_FRONTEND_URL + "/forgot-password?token=" + token
+    )
+    return (
+        '<h1>Welcome to FastSurvey!</h1>' +
+        '<p>Please set a new password via the following link:</p>' +
+        f'<a href="{verification_url}" target="_blank">{verification_url}</a>' +
+        '<p>Best,<br/>The FastSurvey Team</p>' +
+        '<p><br/>If you have not signed up for this ' +
+        'service, you can just ignore this email.</p>'
+    )
