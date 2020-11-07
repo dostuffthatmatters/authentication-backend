db6f9e7ed55e6f6748bc1117c6c3841ea948437b
Felix Böhm <felix@felixboehm.dev>
2020-11-07 17:48:29 +0100

    Rework mailing module to eliminate circular imports and for better code structure

--- app/utilities/mailing.py
+++ app/utilities/mailing.py
@@ -1,58 +1,59 @@
-
-from app import httpx_client, ENVIRONMENT, ADMIN_FRONTEND_URL
-
-
-EMAIL_APPENDIX = (
-    '<p>Best, the FastSurvey team</p>' +
-    '<p><br/>If you have not signed up for this ' +
-    'service, you can just ignore this email.</p>'
-)
+import os
+import httpx
 
 
-async def send_verification_mail(db_account):
-    data = {
-        'from': 'FastSurvey <noreply@fastsurvey.io>',
-        'to': db_account["email"],
-        'subject': 'Verify your FastSurvey account',
-        'html': verification_email_html(db_account),
-        'o:testmode': ENVIRONMENT == 'testing'
-    }
-    response = await httpx_client.post('/messages', data=data)
-    assert(response.status_code == 200), f"{response}"
+# development / production / testing environment
+ENV = os.getenv('ENV')
+# frontend url
+FURL = os.getenv('FURL')
+# mailgun api key
+MGKEY = os.getenv('MGKEY')
 
 
-def verification_email_html(db_account):
-    assert("email_token" in db_account)
-    verification_url = (
-        ADMIN_FRONTEND_URL + "/verify?token=" + db_account["email_token"]
-    )
-    return (
-        '<h1>Welcome to FastSurvey!</h1>' +
-        '<p>Please verify this email via the following link:</p>' +
-        f'<a href="{verification_url}" target="_blank">{verification_url}</a>' +
-        EMAIL_APPENDIX
-    )
+class Letterbox:
+    """Well ... you post it, and sometimes it arrives where it should."""
 
+    def __init__(self):
+        """Create a general email client to be used by all surveys."""
+        self.domain = 'fastsurvey.io'
+        self.sender = f'FastSurvey <noreply@{self.domain}>'
+        self.client = httpx.AsyncClient(
+            auth=('api', MGKEY),
+            base_url=f'https://api.eu.mailgun.net/v3/email.{self.domain}',
+        )
 
-async def send_forgot_password_mail(email: str, token: str):
-    data = {
-        'from': 'FastSurvey <noreply@fastsurvey.io>',
-        'to': email,
-        'subject': 'Restore your FastSurvey password',
-        'html': forgot_password_email_html(token),
-        'o:testmode': ENVIRONMENT == 'testing'
-    }
-    response = await httpx_client.post('/messages', data=data)
-    assert(response.status_code == 200), f"{response}"
+    async def send(self, receiver: str, subject: str, html: str):
+        """Send an email to the given receiver."""
+        data = {
+            'from': self.sender,
+            'to': f'test@{self.domain}' if ENV != 'production' else receiver,
+            'subject': subject,
+            'html': html,
+            'o:testmode': ENV == 'testing',
+            'o:tag': [f'{ENV} transactional'],
+        }
+        response = await self.client.post('/messages', data=data)
+        return response.status_code
 
+    async def send_verification_email(self, receiver: str, token: str):
+        """Send confirmation email in order to verify an email address."""
+        subject = 'Welcome to FastSurvey!'
+        vurl = f'{FURL}/verify?token={token}'  # verification url
+        html = (
+            '<p>Welcome to FastSurvey!</p>'
+            + '<p>We hope that you\'ll gain some interesting insights with your surveys.</p>'
+            + f'<p>Please verify your email address by <a href="{vurl}" target="_blank">clicking here</a>.</p>'
+            + '<p>Best, the FastSurvey team</p>'
+        )
+        return await self.send(receiver, subject, html)
 
-def forgot_password_email_html(token: str):
-    verification_url = (
-        ADMIN_FRONTEND_URL + "/set-password?token=" + token
-    )
-    return (
-        '<h1>Restore your FastSurvey account</h1>' +
-        '<p>Please set a new password via the following link:</p>' +
-        f'<a href="{verification_url}" target="_blank">{verification_url}</a>' +
-        EMAIL_APPENDIX
-    )
+    async def send_reset_password_email(self, receiver: str, token: str):
+        """Send email in order to reset the password of an existing account."""
+        subject = 'Reset Your FastSurvey Password'
+        rurl = f'{FURL}/set-password?token={token}'  # reset url
+        html = (
+            '<p>Hi there,</p>'
+            + f'<p>You can set your new password by <a href="{rurl}" target="_blank">clicking here</a>.</p>'
+            + '<p>Best, the FastSurvey team</p>'
+        )
+        return await self.send(receiver, subject, html)
